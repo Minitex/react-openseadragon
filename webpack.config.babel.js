aa1db58ce2dacb22740afb27a698ed61ca618128
chadfennell <libsys@gmail.com>
2017-02-02 15:47:27 -0600

    webpack with umd module loading and minification

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -8,6 +8,8 @@ var TARGET = process.env.npm_lifecycle_event;
 // e.g. nmp run start (event -- start)
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+var minimize = process.argv.indexOf('--minimize')
+
 var common = {
   module: {
     loaders: [
@@ -16,8 +18,7 @@ var common = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
-          presets: ["react", "es2015", "stage-0"],
-          plugins: ["transform-es2015-modules-umd"]
+          presets: ["react", "es2015", "stage-0"]
         }
       },
       {
@@ -35,14 +36,36 @@ var common = {
   ]
 };
 
+
 if (TARGET === 'build') {
-  module.exports = merge(common, {
-    entry: './src/react-openseadragon.js',
-    output: {
-      path: './lib/',
-      filename: 'react-openseadragon.js'
-    }
-  });
+  if (minimize > 0) {
+    common.plugins.push(new webpack.optimize.UglifyJsPlugin());    
+    var config = 
+      {
+        entry: './src/react-openseadragon.js',
+        output: {
+          path: './dist/',
+          filename: 'react-openseadragon-min.js',
+          library: "react-openseadragon",
+          libraryTarget: 'umd',
+          umdNamedDefine: true
+        }
+      }
+  } else {
+    var config = 
+      {
+        devtool: 'source-map',
+        entry: './src/react-openseadragon.js',
+        output: {
+          path: './dist/',
+          filename: 'react-openseadragon.js',
+          library: "react-openseadragon",
+          libraryTarget: 'umd',
+          umdNamedDefine: true
+        }
+      }
+  }
+  module.exports = merge(common, config);
 }
 
 
@@ -50,7 +73,7 @@ if (TARGET === 'start') {
   module.exports = merge(common, {
     devtool: 'source-map',
     entry: {
-      bundle: ["./src/react-openseadragon.js"],
+      bundle: ["./src/[name].js"],
       example: "./example/example.js"
     },
     output: {
@@ -59,3 +82,5 @@ if (TARGET === 'start') {
     }
   });
 }
+
+
