8829758e0ccc022de7a3a44f8da56946169f7c5b
chadfennell <libsys@gmail.com>
2017-02-15 11:49:46 -0600

    don't show the table of contents on single items

--- src/react-openseadragon-nav.js
+++ src/react-openseadragon-nav.js
@@ -13,27 +13,31 @@ export default class OpenSeadragonNav extends React.Component {
 
     render() {
       const { tocs, id } = this.props
-      return (
-                <div className="row image-nav">
-                  <ul className="nav nav-pills">
-                    <li>
-                      <div className="toc-select">
-                        <label htmlFor="toc">Table of Contents:</label>
-                        <select name="toc" onChange={this.handleChange}>
-                          {tocs.map(function(toc, i) {
-                            let page = i + 1
-                            if (id == i) {
-                              return <option value={i} key={i} selected="selected">{page}. {toc}</option>
-                            } else {
-                              return <option value={i} key={i}>{page}. {toc}</option>
-                            }
-                          })}
-                        </select>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              )
+      if (tocs.length > 1) {
+        return (
+                  <div className="row image-nav">
+                    <ul className="nav nav-pills">
+                      <li>
+                        <div className="toc-select">
+                          <label htmlFor="toc">Table of Contents:</label>
+                          <select name="toc" onChange={this.handleChange}>
+                            {tocs.map(function(toc, i) {
+                              let page = i + 1
+                              if (id == i) {
+                                return <option value={i} key={i} selected="selected">{page}. {toc}</option>
+                              } else {
+                                return <option value={i} key={i}>{page}. {toc}</option>
+                              }
+                            })}
+                          </select>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                )
+      } else {
+        return <span />
+      }
     }
 
 }
