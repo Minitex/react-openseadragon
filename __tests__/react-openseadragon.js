a5bd73c8b9a186a5173a9c299f9ec1e0cd80a45d
chadfennell <libsys@gmail.com>
2017-02-22 14:17:51 -0600

    get snapshot testing working

--- __tests__/react-openseadragon.js
+++ __tests__/react-openseadragon.js
@@ -7,36 +7,30 @@ jest.mock('../src/react-openseadragon-viewer', () => 'ReactOpenSeadragonViewer')
 describe('OpenSeadragon', () => {
   it('should render correctly', () => {
 
-      var items = [
-        {
-          "type": "image",
-          "label": "Image",
-          "focus": true,
-          "include_controls": true,
-          "sequenceMode":  true,
-          "showReferenceStrip": true,
-          "defaultZoomLevel": 0,
-          "tileSources":   [
-              "https://stacks.stanford.edu/image/iiif/hg676jb4964%2F0380_796-44/info.json",
-              "https://ids.lib.harvard.edu/ids/iiif/25286610/info.json"
-          ]
-        },
-        {
-          "type": "transcript",
-          "label": "Transcript",
-          "texts": ["First Item", "Second Item"],
-          "focus": false            
-        }
-      ]
-
-      var tocs = [
+  var config =
+    {
+      "type": "image",
+      "label": "Image",
+      "focus": true,
+      "include_controls": true,
+      "sequenceMode":  true,
+      "showReferenceStrip": true,
+      "defaultZoomLevel": 0,
+      "tileSources":   [
+        "https://stacks.stanford.edu/image/iiif/hg676jb4964%2F0380_796-44/info.json",
+        "https://ids.lib.harvard.edu/ids/iiif/25286610/info.json"
+      ],
+      tocs: [
         "MLK",
         "A Statue"
       ]
+    }
 
     const component = renderer.create(
-      <OpenSeadragon tocs={tocs} items={items} />
+      <OpenSeadragon config={config} base_path="/" />
     )
-    expect(component.toJSON()).toMatchSnapshot()
+    var json_component = component.toJSON();
+    json_component.children[1].props.location.key = 'fake_key';
+    expect(json_component).toMatchSnapshot()
   })
 })
--- __tests__/single-toc.js
+++ __tests__/single-toc.js
@@ -7,35 +7,29 @@ jest.mock('../src/react-openseadragon-viewer', () => 'ReactOpenSeadragonViewer')
 describe('OpenSeadragon', () => {
   it('should render correctly', () => {
 
-      var items = [
-        {
-          "type": "image",
-          "label": "Image",
-          "focus": true,
-          "include_controls": true,
-          "sequenceMode":  true,
-          "showReferenceStrip": true,
-          "defaultZoomLevel": 0,
-          "tileSources":   [
-              "https://stacks.stanford.edu/image/iiif/hg676jb4964%2F0380_796-44/info.json",
-              "https://ids.lib.harvard.edu/ids/iiif/25286610/info.json"
-          ]
-        },
-        {
-          "type": "transcript",
-          "label": "Transcript",
-          "texts": ["First Item", "Second Item"],
-          "focus": false            
-        }
-      ]
-
-      var tocs = [
+var config =
+    {
+      "type": "image",
+      "label": "Image",
+      "focus": true,
+      "include_controls": true,
+      "sequenceMode":  true,
+      "showReferenceStrip": true,
+      "defaultZoomLevel": 0,
+      "tileSources":   [
+        "https://stacks.stanford.edu/image/iiif/hg676jb4964%2F0380_796-44/info.json",
+        "https://ids.lib.harvard.edu/ids/iiif/25286610/info.json"
+      ],
+      tocs: [
         "MLK"
       ]
+    }
 
     const component = renderer.create(
-      <OpenSeadragon tocs={tocs} items={items} />
-    )
+      <OpenSeadragon config={config} base_path="/" />
+    );
+    var json_component = component.toJSON();
+    json_component.children[1].props.location.key = 'fake_key';
     expect(component.toJSON()).toMatchSnapshot()
   })
 })
