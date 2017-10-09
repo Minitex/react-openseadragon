b3ad0d984f0107151eed52465b31c33fe8e6d568
chadfennell <libsys@gmail.com>
2017-10-09 15:30:37 -0500

    latest build

--- example/example.js
+++ example/example.js
@@ -9,15 +9,23 @@ const config =
     focus: true,
     include_controls: true,
     sequenceMode: true,
-    showReferenceStrip: true,
+    showReferenceStrip: false,
     defaultZoomLevel: 0,
     tileSources: [
       'https://stacks.stanford.edu/image/iiif/hg676jb4964%2F0380_796-44/info.json',
       'https://ids.lib.harvard.edu/ids/iiif/25286610/info.json',
     ],
-    tocs: [
+    transcripts: [
+      'MLK at at thing',
+      'A statue here, very statuey',
+    ],
+    thumbnails: [
+      'https://stacks.stanford.edu/image/iiif/hg676jb4964%2F0380_796-44/full/100,/0/default.jpg',
+      'https://ids.lib.harvard.edu/ids/iiif/25286610/full/100,/0/native.jpg',
+    ],
+    pages: [
       'MLK',
-      'A Statue',
+      'Statue',
     ],
   };
 
